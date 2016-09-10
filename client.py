15b3bcd85845beae616572bb1f2012f2f5a8029b
robinonsay <raomax@gmail.com>
2016-09-10 11:42:36 -0400

    Running a socketIO library

--- client.py
+++ client.py
@@ -1,25 +1,14 @@
-from socket import *
+from socketIO_client import SocketIO, LoggingNamespace
 
 host = "li1013-216.members.linode.com"
-
-print host
-
 port = 5000
 
-s = socket(AF_INET, SOCK_STREAM)
+socketIO = SocketIO(host, port, LoggingNamespace)
+print host
+print port
 
-print "socket made"
-
-s.connect((host,port))
-
-print "socket connected!!!"
-s.send("Python is Connected")
+socketIO.on('connection', on_connection)
 
 
-while 1:
-    data = s.recv(1024)
-    print data
-    if not data:
-        break
-    s.send(data)
-s.close()                    # Close the socket when done
+def on_connection(*args):
+    print('connected', args)
