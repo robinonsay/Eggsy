3aea9c68b93ff91a265b5f15092615751192dd44
robinonsay <raomax@gmail.com>
2016-09-10 11:47:38 -0400

    Created timeout

--- client.py
+++ client.py
@@ -4,10 +4,11 @@ host = "li1013-216.members.linode.com"
 port = 5000
 
 socketIO = SocketIO(host, port, LoggingNamespace)
+socketIO.wait()
 print host
 print port
 
 def on_connection(*args):
     print('connected', args)
-    
+
 socketIO.on('connection', on_connection)
