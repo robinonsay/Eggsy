2edb69ed01aec5cdc6f37801eaba0c461339e252
robinonsay <raomax@gmail.com>
2016-09-10 16:52:26 -0400

    Added b

--- app.py
+++ app.py
@@ -10,9 +10,9 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app)
 
-arduino_conn = serial.Serial(0, 9600, timeout=1)
-# while arduino_conn.inWaiting()>0:
-#     arduino_conn.read(1)
+arduino_conn = serial.Serial("/dev/ttyAMA0", 9600, timeout=1)
+while arduino_conn.inWaiting()>0:
+    arduino_conn.read(1)
 cook = False
 
 @app.route('/')
@@ -25,7 +25,7 @@ def cook():
     print("Cook Egg Before")
     global cook
     cook = True
-    arduino_conn.write(b"1")
+    arduino_conn.write("1\r")
     print("Cook Egg After")
     return "Cooking an Egg"
 
