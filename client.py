3c038d986e176c724ffd39673e2358f91d0e5234
robinonsay <raomax@gmail.com>
2016-09-10 10:02:55 -0400

    Stackoverflow code

--- client.py
+++ client.py
@@ -14,5 +14,18 @@ s.connect((host,port))
 
 print "socket connected!!!"
 
-print s.recv(8192)
+nreceive = True#nreceive = Not Received
+ticks = 0
+f = None
+while nreceive and ticks < 101:#try to get the info 100 times or until it's received
+    ticks+=1
+    try:
+        f = mysocket.makefile('rb')
+        if not f == None:
+            nreceive = False
+    except:
+        pass
+data = f.read(1024)
+
+print data
 s.close                     # Close the socket when done
