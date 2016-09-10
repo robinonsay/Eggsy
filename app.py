321cedfae8796603ae5fe2477aacd5630e30fcfe
robinonsay <raomax@gmail.com>
2016-09-10 16:04:14 -0400

    Fixed thread issue

--- app.py
+++ app.py
@@ -34,8 +34,8 @@ def serial_comm():
         else:
             time.sleep(2)
 
-t1 = Thread.join("main_routine", timeout=None)
-t2 = Thread.join("serial_comm", timeout=None)
+t1 = Thread.join(main_routine, timeout=None)
+t2 = Thread.join(serial_comm, timeout=None)
 
 t1.start()
 t2.start()
